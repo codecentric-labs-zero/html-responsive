dc6807c01aeacffba778f149cca74cd92cbba131
Milan Šušnjar <milan.susnjar@codecentric.de>
2016-09-14 15:13:58 +0200

    change output file name and include css and js in html

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 
 var config = {
     bootstrapDir: './bower_components/bootstrap-sass',
+    jqueryDir: './bower_components/jquery',
 	styleDir: './resources/scss/*.scss',
     publicDir: './dist',
 };
 
 gulp.task('css', function() {
-    return gulp.src('./resources/scss/style.scss')
+    return gulp.src('./resources/scss/main.scss')
     .pipe(sass({
         includePaths: [config.bootstrapDir + '/assets/stylesheets']
     }))
+    .pipe(rename('app.css'))
     .pipe(gulp.dest(config.publicDir + '/css'));
+});
+
+gulp.task('js', function() {
+    return gulp.src([config.jqueryDir + '/dist/jquery.min.js', config.bootstrapDir + '/assets/javascripts/bootstrap.min.js'])
+        .pipe(gulp.dest(config.publicDir + '/js'));
 });
 
 gulp.task('fonts', function() {
@@ -24,4 +32,4 @@ gulp.task('watch', function() {
 	gulp.watch(config.styleDir, ['css']);
 });
 
-gulp.task('default', ['css', 'fonts']);
+gulp.task('default', ['css', 'js', 'fonts']);
